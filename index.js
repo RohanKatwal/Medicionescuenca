22678b1213679ebb4abc79308c6f8ce1ea45bd6f
Ronish Katuwal <ronishkatuwal00@gmail.com>
2025-10-29 23:04:51 +0545

    added active status in menu

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 function toggleMenu() {
     document.querySelector("#navLinks").classList.toggle("show");
 }
+
+const navItems = document.querySelectorAll(".link");
+const activeLine = document.querySelectorAll(".active-line");
+
+navItems.forEach((nav, index) => {
+  nav.addEventListener("click", (e) => {
+    navItems.forEach((item) => item.classList.remove("active"));
+    nav.classList.add("active");
+
+    activeLine.forEach((item) => item.classList.remove('active'));
+    activeLine[index].classList.add("active");
+  });
+});
