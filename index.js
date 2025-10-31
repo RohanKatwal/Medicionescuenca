c72189b1dec6eced0b7d064b2599599d8c3e047d
Rohan Katwal <rohanwork909@gmail.com>
2025-10-31 18:07:17 +0545

    added image animation

--- index.js
+++ index.js
@@ -14,3 +14,18 @@ navItems.forEach((nav, index) => {
     activeLine[index].classList.add("active");
   });
 });
+
+
+const images = [
+    "/images/poster.jpg",
+    "/images/logo.png",
+    "/images/IC_logo.png",
+  ];
+
+  let index = 0;
+  const imgElement = document.getElementById("changingImage");
+
+  setInterval(() => {
+    index = (index + 1) % images.length; // loop through images
+    imgElement.src = images[index];
+  }, 2000);
